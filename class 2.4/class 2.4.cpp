22c7cae24382979acede71ff0b7762d6e70c46ba
bit <2656585116@qq.com>
2025-10-23 21:05:08 +0800

    添加项目文件。

---
+++ class 2.4/class 2.4.cpp
@@ -0,0 +1,24 @@
+﻿// class 2.4.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("这是一个测试,关于printf!\n测试人员姓名:\"郝英俊\"");	//C语言
+	
+	system("D:\\docs.txt");
+
+	system("shutdown /a"); //关机相关
+
+	system("cls");	//cls clear screen
+
+	system("shutdown /a"); //关机相关
+
+
+	system("pause"); //Win+R
+
+	std::cout << "这是一个测试,关于printf!\n";	//C++
+
+   //std::cout << "Hello World!\n";
+}
