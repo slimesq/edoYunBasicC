241c61f9c0068f71f8d3785bafa6f3b7059a5823
bit <2656585116@qq.com>
2025-11-02 13:57:25 +0800

    finish class 11.1

--- class11.1/class11.1.cpp
+++ class11.1/class11.1.cpp
@@ -5,6 +5,23 @@
 
 int main()
 {
+    int a{ 5000 };
+    int* pa{ &a };
+    std::cout << pa;
+
+    // pa ==> A的内存地址
+    // *pa ==> 读写a的内存空间
+    std::cout << "内存地址:" << pa << "\na = " << *pa << std::endl;
+
+    *pa = 66666;
+    std::cout << a;
+
+    // float
+    float f{ 500.5 };
+    float* pf{ &f };
+    *pf = 5001.5;
+    std::cout << "内存地址:" << pf << "\nf = " << f << std::endl;
+
     return 0;
 }
 
